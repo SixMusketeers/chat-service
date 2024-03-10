2f26b224aee7785b01dec518cd5117fa31f712c4
abhay7321 <gaikwadab7@gmail.com>
2024-03-10 18:19:53 +0530

    Added method to save data to sql db

--- src/main/java/com/chat/app/Controller/MainController.java
+++ src/main/java/com/chat/app/Controller/MainController.java
@@ -1,8 +1,10 @@
 package com.chat.app.Controller;
 
 import com.chat.app.Model.Message;
-import com.chat.app.Repository.MessageRepo;
+import com.chat.app.Model.User;
+import com.chat.app.Repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,13 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
+    private final String ANALYTICS_COLLECTION = "message";
+
     @Autowired
-    MessageRepo messageRepo;
+    MongoTemplate mongoTemplate;
+
+    @Autowired
+    UserRepo userRepo;
 
     @PostMapping("/addMessage")
     public void addMessage(@RequestBody Message message){
         System.out.println("Before saving");
-        messageRepo.save(message);
+        mongoTemplate.insert(message, ANALYTICS_COLLECTION);
+        System.out.println("After Saving");
+    }
+
+    @PostMapping("/addUserToSQL")
+    public void addMessageToSQL(@RequestBody User user){
+        System.out.println("Before saving");
+        userRepo.save(user);
         System.out.println("After Saving");
     }
 }
---
+++ src/main/java/com/chat/app/config/MongoConfiguration.java
@@ -0,0 +1,2 @@
+package com.chat.app.config;public class MongoConfiguration {
+}
