36fd49098c69ba507faceaab752f2b098ff30e2b
abhay7321 <gaikwadab7@gmail.com>
2024-03-10 18:22:19 +0530

    Added mongo configuration

--- src/main/java/com/chat/app/Model/Message.java
+++ src/main/java/com/chat/app/Model/Message.java
@@ -1,7 +1,6 @@
 package com.chat.app.Model;
 
 import jakarta.persistence.Id;
-import jdk.jfr.DataAmount;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
--- src/main/java/com/chat/app/config/MongoConfiguration.java
+++ src/main/java/com/chat/app/config/MongoConfiguration.java
@@ -1,2 +1,20 @@
-package com.chat.app.config;public class MongoConfiguration {
+package com.chat.app.config;
+
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.convert.DefaultMongoTypeMapper;
+import org.springframework.data.mongodb.core.convert.MappingMongoConverter;
+
+public class MongoConfiguration {
+
+    // The purpose of this class is to simply remove the _class field from the collections on insert
+    @Autowired
+    private MappingMongoConverter mappingMongoConverter;
+
+    // remove _class
+    @PostConstruct
+    public void setUpMongoEscapeCharacterConversion() {
+        mappingMongoConverter.setTypeMapper(new DefaultMongoTypeMapper(null));
+    }
+
 }
