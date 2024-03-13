3cba3f9660d9ffac5bb3c1c78afb249fe8e68e9d
nisarg <nisarggogate212@gmail.com>
2024-03-13 22:42:55 +0530

    Created service to store messages in message History

--- src/main/java/com/chat/app/Controller/MainController.java
+++ src/main/java/com/chat/app/Controller/MainController.java
@@ -1,25 +1,36 @@
 package com.chat.app.Controller;
 
-import com.chat.app.Model.ChatHistory;
 import com.chat.app.Model.Message;
 import com.chat.app.Model.User;
 import com.chat.app.Repository.UserRepo;
+import com.chat.app.config.ChatConstants;
+import com.chat.app.services.MessageHistoryService;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Objects;
+
 @RestController
+@NoArgsConstructor
+@AllArgsConstructor
 public class MainController {
 
-    private final String ANALYTICS_COLLECTION = "message";
+    @Autowired
+    MessageHistoryService messageHistoryService;
 
     @Autowired
     MongoTemplate mongoTemplate;
 
-    @Autowired
-    UserRepo userRepo;
+    private static final Logger logger = LoggerFactory.getLogger(MainController.class);
+
+    private UserRepo userRepo;
 
     @PostMapping("/addMessage")
     public void addMessage(@RequestBody Message message){
@@ -35,6 +46,33 @@ public class MainController {
 //        mongoTemplate.insert(chatHistory, "chat_history");
 
         System.out.println("After Saving");
+    }
+
+    @PostMapping("/sendMessage")
+    public String sendMessage(@RequestBody Message message) {
+
+        try {
+            // More validation if needed
+            if (Objects.isNull(message.getChannelId()) || message.getChannelId().isEmpty()) {
+                throw new IllegalArgumentException("Channel ID cannot be empty");
+            }
+
+            // Store message in message document
+            message = mongoTemplate.insert(message, ChatConstants.MESSAGE);
+            logger.info("*** Message Sent successfully *** " + message.getId());
+
+
+            // Store message in chat history document
+            messageHistoryService.persistMessageHistory(message);
+            logger.info("*** Message Received successfully ***" + message.getId());
+
+        } catch (IllegalStateException ex) {
+            return ex.getMessage();
+        } catch (Exception ex) {
+            logger.error(ex.getMessage());
+            return "Internal Server Error";
+        }
+        return "Message Sent successfully";
     }
 
     @PostMapping("/addUserToSQL")
